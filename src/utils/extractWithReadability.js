9c44c8cdcc463353482e7bf343c4a9bfff1fddc4
Dave Schumaker <daveschumaker@users.noreply.github.com>
2022-03-31 15:45:21 -0700

    feat: convert some tags before handing off to Readability.

--- src/utils/extractWithReadability.js
+++ src/utils/extractWithReadability.js
@@ -11,7 +11,27 @@ import isHTMLString from './isHTMLString.js'
  */
 export default (html, inputUrl = '') => {
   if (!isHTMLString(html)) return null
-  const doc = new DOMParser().parseFromString(html, 'text/html')
+
+  // TODO: This should probably be extracted from out of this function and
+  // run before passing data into extractWithReadability. That way, we can
+  // better transform tags that things like Readability might exclude.
+  let doc = new DOMParser().parseFromString(html, 'text/html')
+
+  // Fix issues with "aside" tags on websites such as Engadget and The Verge,
+  // where they embed quotes within the actual article.
+  doc.querySelectorAll('aside').forEach(node => {
+    const newNode = doc.createElement('div')
+    newNode.innerHTML = node.innerHTML
+    node.parentNode.replaceChild(newNode, node)
+  })
+
+  // Convert "q" tags into blockquotes.
+  doc.querySelectorAll('q').forEach(node => {
+    const newNode = doc.createElement('blockquote')
+    newNode.innerHTML = node.innerHTML
+    node.parentNode.replaceChild(newNode, node)
+  })
+
   const base = doc.createElement('base')
   base.setAttribute('href', inputUrl)
   doc.head.appendChild(base)
